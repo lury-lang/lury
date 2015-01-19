6ae66e0670dcaf23435b71d6d482167bb979e3d9
Tomona Nanase <nanase@users.noreply.github.com>
2015-01-20 00:24:02 +0900

    TokenizedTokenクラスにXMLドキュメントを追加

--- src/Lury/Compiling/Lexer/TokenizedToken.cs
+++ src/Lury/Compiling/Lexer/TokenizedToken.cs
@@ -29,6 +29,9 @@ using System;
 
 namespace Lury.Compiling
 {
+    /// <summary>
+    /// トークン化され、ソースコードにおける位置が割り当てられたトークンを格納します。
+    /// </summary>
     class TokenizedToken : yyParser.IToken
     {
         #region -- Private Fields --
@@ -42,18 +45,41 @@ namespace Lury.Compiling
 
         #region -- Public Properties --
 
+        /// <summary>
+        /// トークンの文字列を取得します。
+        /// </summary>
+        /// <value>トークンの文字列。</value>
         public string Text { get { return this.text; } }
 
+        /// <summary>
+        /// トークンの種類を表すトークンナンバーを取得します。
+        /// </summary>
+        /// <value>トークンの種類を表すトークンナンバー。</value>
         public int TokenNumber { get { return this.tokenNumber; } }
 
+        /// <summary>
+        /// トークンのインデントレベルを取得します。
+        /// </summary>
+        /// <value>トークンのインデントレベル。</value>
         public int IndentLevel { get { return this.indentLevel; } }
 
+        /// <summary>
+        /// トークンが出現したソースコードでのインデックス位置を取得します。
+        /// </summary>
+        /// <value>インデックス位置。</value>
         public int Index { get { return this.index; } }
 
         #endregion
 
         #region -- Constructors --
 
+        /// <summary>
+        /// 新しい <see cref="Lury.Compiling.TokenizedToken"/> クラスのインスタンスを初期化します。
+        /// </summary>
+        /// <param name="text">トークン文字列。</param>
+        /// <param name="tokenNumber">トークンナンバー。</param>
+        /// <param name="indentLevel">インデントレベル。</param>
+        /// <param name="index">インデックス位置。</param>
         public TokenizedToken(string text, int tokenNumber, int indentLevel, int index)
         {
             this.text = text;
@@ -66,6 +92,10 @@ namespace Lury.Compiling
 
         #region -- Public Methods --
 
+        /// <summary>
+        /// 現在の <see cref="Lury.Compiling.TokenizedToken"/> インスタンスと等価な表現の文字列に変換します。
+        /// </summary>
+        /// <returns>現在のインスタンスを表す等価な文字列。</returns>
         public override string ToString()
         {
             return string.Format("{0}> {1} : \"{2}\"", this.indentLevel, this.tokenNumber, this.text);
