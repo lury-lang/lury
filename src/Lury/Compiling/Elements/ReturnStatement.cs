b674e9ebbf3a349386e043eca8d963b38bdc6f95
Tomona Nanase <nanase@users.noreply.github.com>
2015-08-05 15:07:26 +0900

    Add ReturnStatement.cs

---
+++ src/Lury/Compiling/Elements/ReturnStatement.cs
@@ -0,0 +1,57 @@
+﻿//
+// ReturnStatement.cs
+//
+// Author:
+//       Tomona Nanase <nanase@users.noreply.github.com>
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Tomona Nanase
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+namespace Lury.Compiling.Elements
+{
+    class ReturnStatement : SimpleStatement
+    {
+        public readonly Lexer.Token Return;
+        public readonly Expression Expression;
+
+        public ReturnStatement(object @return, object expression)
+        {
+            this.Return = (Lexer.Token)@return;
+            this.Expression = (Expression)expression;
+        }
+
+        public ReturnStatement(object @return)
+            : this(@return, null)
+        {
+        }
+
+        public override string ToString()
+        {
+            if (this.Expression != null)
+                return string.Format("{0} {1}", this.Return, this.Expression);
+            else
+                return this.Return.ToString();
+        }
+    }
+}
