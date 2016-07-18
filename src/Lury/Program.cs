156c9d65861ed40a6cb93e695b650cdc3be4d696
Tomona Nanase <nanase@users.noreply.github.com>
2016-07-18 22:26:23 +0900

    add: Luryコンソールを追加

---
+++ src/Lury/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using Lury.Core.Runtime;
+
+namespace Lury
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "-i")
+            {
+                using (var stream = new StreamReader(Console.OpenStandardInput()))
+                    LuryEngine.Run(stream.ReadToEnd());
+            }
+            else
+                RunAsIntaractiveMode();
+        }
+
+        private static void RunAsIntaractiveMode()
+        {
+            Console.WriteLine("Lury 0.3.0 (< Spec 0.3 phase 1), running on {0}", Environment.Is64BitProcess ? "x64" : "x86");
+            var engine = new LuryEngine();
+
+            while (true)
+            {
+                var multiline = false;
+                var emptyLine = 0;
+                var input = string.Empty;
+
+                do
+                {
+                    Console.Write(input == string.Empty ? ">>> " : "... ");
+                    var line = Console.ReadLine();
+
+                    if (line == null)
+                        return;
+
+                    if (line.TrimEnd().EndsWith(":"))
+                        multiline = true;
+
+                    input += line + "\n";
+                    emptyLine++;
+
+                } while (multiline && emptyLine < 3);
+
+                try
+                {
+                    var obj = engine.Execute(input);
+
+                    if (obj == null)
+                        continue;
+
+                    Console.WriteLine("{0}", obj);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: {0}", ex.Message);
+                }
+            }
+        }
+    }
+}
