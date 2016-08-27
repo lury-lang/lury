eefde23046bd52d8da77f072a79b2d089e8810be
Tomona Nanase <nanase@users.noreply.github.com>
2016-08-27 17:24:06 +0900

    :green_heart: ユニットテスト更新

--- test/Unittest/LuryObjectTest.cs
+++ test/Unittest/LuryObjectTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Lury.Core.Error;
 using Lury.Core.Runtime;
 using NUnit.Framework;
 
@@ -36,6 +37,37 @@ namespace Unittest
 
             Assert.False(baseObject.Has("foo"));
             Assert.False(baseObject.Has("bar"));
+        }
+
+        [Test]
+        public void FetchTest()
+        {
+            var baseObject = new LuryObject(null, null);
+            var classObject = new LuryObject(baseObject, null, "BaseClass");
+            var luryObject = new LuryObject(baseObject, classObject);
+
+            var attributeObject1 = new LuryObject(baseObject, null, 1);
+            var attributeObject2 = new LuryObject(baseObject, null, 2);
+            var attributeObject3 = new LuryObject(baseObject, null, 3);
+            var attributeObject4 = new LuryObject(baseObject, null, 4);
+
+            luryObject.Assign("foo", attributeObject1);
+            classObject.Assign("bar", attributeObject2);
+            baseObject.Assign("hoge", attributeObject3);
+            baseObject.Assign("fuga", attributeObject4);
+
+            Assert.AreEqual(attributeObject1, luryObject.Fetch("foo"));
+            Assert.AreEqual(attributeObject3, luryObject.Fetch("hoge"));
+            Assert.AreEqual(attributeObject4, luryObject.Fetch("fuga"));
+
+            Assert.AreEqual(attributeObject3, baseObject.Fetch("hoge"));
+            Assert.AreEqual(attributeObject4, baseObject.Fetch("fuga"));
+
+            Assert.Throws<AttributeNotDefinedException>(() => luryObject.Fetch("bar"));
+            Assert.Throws<AttributeNotDefinedException>(() => luryObject.Fetch("baz"));
+
+            Assert.Throws<AttributeNotDefinedException>(() => baseObject.Fetch("foo"));
+            Assert.Throws<AttributeNotDefinedException>(() => baseObject.Fetch("bar"));
         }
     }
 }
