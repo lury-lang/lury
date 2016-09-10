885098a3b65cc399bc41bc9ce8d31f1b729d507a
Tomona Nanase <nanase@users.noreply.github.com>
2016-09-10 22:39:52 +0900

    :green_heart: LuryContextに対するユニットテストを追加

---
+++ test/Unittest/LuryContextTest.cs
@@ -0,0 +1,44 @@
+﻿using Lury.Core.Runtime;
+using NUnit.Framework;
+
+namespace Unittest
+{
+    [Parallelizable]
+    [TestFixture]
+    public class LuryContextTest
+    {
+        [Test]
+        public void CtorTest()
+        {
+            var context1 = new LuryContext();
+            var context2 = new LuryContext(context1);
+
+            Assert.That(context1.Parent, Is.Null);
+            Assert.That(context2.Parent, Is.EqualTo(context1));
+        }
+
+        [Test]
+        public void AssignTest()
+        {
+            var context1 = new LuryContext();
+            var context2 = new LuryContext(context1);
+            var context3 = new LuryContext(context2);
+            var object1 = new LuryObject(null, null, 1);
+            var object2 = new LuryObject(null, null, 2);
+            var object3 = new LuryObject(null, null, 3);
+
+            context1["foo"] = object1;
+            context2["bar"] = object2;
+            context3["hoge"] = object3;
+
+            Assert.That(context1.Has("foo"));
+            Assert.That(context2.Has("foo"));
+            Assert.That(context3.Has("foo"));
+
+            Assert.That(context2.Has("bar"));
+            Assert.That(context3.Has("bar"));
+
+            Assert.That(context3.Has("hoge"));
+        }
+    }
+}
