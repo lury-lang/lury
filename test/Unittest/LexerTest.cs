a6ffe749ed082cb51ef3ac12ea472863003d8b1a
Tomona Nanase <nanase@users.noreply.github.com>
2016-10-08 23:35:40 +0900

    :sparkles: LexerTestを追加

---
+++ test/Unittest/LexerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Atn;
+using Lury.Core.Compiler;
+using Lury.Core.Error;
+using Lury.Core.Runtime;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using static Lury.Core.Compiler.LuryLexer;
+
+namespace Unittest
+{
+    [Parallelizable]
+    [TestFixture]
+    public class LexerTest
+    {
+        [Test]
+        public void IntegerTest()
+        {
+            Assert.That(Tokens.From("0").Type, Are.EqualTo(DECIMAL_INTEGER));
+            Assert.That(Tokens.From("0").Text, Are.EqualTo("0"));
+
+            Assert.That(Tokens.From("1").Type, Are.EqualTo(DECIMAL_INTEGER));
+            Assert.That(Tokens.From("1").Text, Are.EqualTo("1"));
+        }
+    }
+}
