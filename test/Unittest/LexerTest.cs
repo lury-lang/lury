0d0b6367c9e1de846954545c5b4c19ed3cdfd8e2
Tomona Nanase <nanase@users.noreply.github.com>
2016-10-09 12:13:41 +0900

    2進数、8進数、16進数に対するテストを追加

--- test/Unittest/LexerTest.cs
+++ test/Unittest/LexerTest.cs
@@ -21,6 +21,22 @@ namespace Unittest
         {
             Assert.That("0", IsTokenized.Under(DECIMAL_INTEGER).And.Append(IsSeparated.Into("0")));
             Assert.That("1", IsTokenized.Under(DECIMAL_INTEGER).And.Append(IsSeparated.Into("1")));
+
+            Assert.That("0b0", IsTokenized.Under(BIN_INTEGER).And.Append(IsSeparated.Into("0b0")));
+            Assert.That("0b1", IsTokenized.Under(BIN_INTEGER).And.Append(IsSeparated.Into("0b1")));
+            Assert.That("0B0", IsTokenized.Under(BIN_INTEGER).And.Append(IsSeparated.Into("0B0")));
+            Assert.That("0B1", IsTokenized.Under(BIN_INTEGER).And.Append(IsSeparated.Into("0B1")));
+
+            Assert.That("0o0", IsTokenized.Under(OCT_INTEGER).And.Append(IsSeparated.Into("0o0")));
+            Assert.That("0o7", IsTokenized.Under(OCT_INTEGER).And.Append(IsSeparated.Into("0o7")));
+            Assert.That("0O0", IsTokenized.Under(OCT_INTEGER).And.Append(IsSeparated.Into("0O0")));
+            Assert.That("0O7", IsTokenized.Under(OCT_INTEGER).And.Append(IsSeparated.Into("0O7")));
+
+            Assert.That("0x0", IsTokenized.Under(HEX_INTEGER).And.Append(IsSeparated.Into("0x0")));
+            Assert.That("0xf", IsTokenized.Under(HEX_INTEGER).And.Append(IsSeparated.Into("0xf")));
+            Assert.That("0X0", IsTokenized.Under(HEX_INTEGER).And.Append(IsSeparated.Into("0X0")));
+            Assert.That("0Xf", IsTokenized.Under(HEX_INTEGER).And.Append(IsSeparated.Into("0Xf")));
+            Assert.That("0XF", IsTokenized.Under(HEX_INTEGER).And.Append(IsSeparated.Into("0XF")));
         }
     }
 }
