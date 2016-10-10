bdbe9b28e4e07ecc23de77bb983ae814afc042cf
Tomona Nanase <nanase@users.noreply.github.com>
2016-10-10 23:09:24 +0900

    :white_check_mark:　リテラルに対するテストを追加

--- test/Unittest/LexerTest.cs
+++ test/Unittest/LexerTest.cs
@@ -21,6 +21,7 @@ namespace Unittest
         {
             Assert.That("0", IsTokenized.Under(DECIMAL_INTEGER).And.Append(IsSeparated.Into("0")));
             Assert.That("1", IsTokenized.Under(DECIMAL_INTEGER).And.Append(IsSeparated.Into("1")));
+            Assert.That("10", IsTokenized.Under(DECIMAL_INTEGER).And.Append(IsSeparated.Into("10")));
 
             Assert.That("0b0", IsTokenized.Under(BIN_INTEGER).And.Append(IsSeparated.Into("0b0")));
             Assert.That("0b1", IsTokenized.Under(BIN_INTEGER).And.Append(IsSeparated.Into("0b1")));
@@ -37,6 +38,33 @@ namespace Unittest
             Assert.That("0X0", IsTokenized.Under(HEX_INTEGER).And.Append(IsSeparated.Into("0X0")));
             Assert.That("0Xf", IsTokenized.Under(HEX_INTEGER).And.Append(IsSeparated.Into("0Xf")));
             Assert.That("0XF", IsTokenized.Under(HEX_INTEGER).And.Append(IsSeparated.Into("0XF")));
+        }
+
+        [Test]
+        public void FloatNumberTest()
+        {
+            Assert.That("0.0", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("0.0")));
+            Assert.That("1.0", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("1.0")));
+            Assert.That("10.0", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("10.0")));
+
+            Assert.That("0.1", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("0.1")));
+            Assert.That("0.01", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("0.01")));
+            Assert.That(".1", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into(".1")));
+
+            Assert.That("0.1e10", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("0.1e10")));
+            Assert.That("0.1E10", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("0.1E10")));
+            Assert.That("0.1e+10", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("0.1e+10")));
+            Assert.That("0.1E+10", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("0.1E+10")));
+            Assert.That("0.1e-10", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("0.1e-10")));
+            Assert.That("0.1E-10", IsTokenized.Under(FLOAT_NUMBER).And.Append(IsSeparated.Into("0.1E-10")));
+        }
+
+        [Test]
+        public void StringEmptyTest()
+        {
+            Assert.That("''", IsTokenized.Under(STRING_LITERAL).And.Append(IsSeparated.Into("''")));
+            Assert.That("\"\"", IsTokenized.Under(STRING_LITERAL).And.Append(IsSeparated.Into("\"\"")));
+            Assert.That("``", IsTokenized.Under(STRING_LITERAL).And.Append(IsSeparated.Into("``")));
         }
     }
 }
